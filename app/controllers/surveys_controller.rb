99c4e9e1d1c5110db892f0df8be05cca2b4ba1f3
Reinhardt <reinhardt.cgr@gmail.com>
2021-10-07 20:50:25 +1300

    removed puts that were used for testing

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,12 +1,10 @@
 class SurveysController < ApplicationController
   def new
     @survey = Survey.new
-    puts '>>> in the SurveysController@new'
   end
 
   def create
-    @survey = Survey.create(survey_params)
-    puts '>>> new Survey Created with Name = ' + @survey.name
+    Survey.create(survey_params)
   end
 
   private
