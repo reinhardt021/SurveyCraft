6e15ff12a9b2c2644c77be32d16dbd4b359041fd
Reinhardt <reinhardt.cgr@gmail.com>
2021-10-07 20:37:57 +1300

    Switched to using strong params int eh controller and removed unnecessary parentheses

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,12 +1,16 @@
 class SurveysController < ApplicationController
   def new
-    @survey = Survey.new()
-    puts 'in the SurveysController@new'
+    @survey = Survey.new
+    puts '>>> in the SurveysController@new'
   end
 
   def create
-    @survey = Survey.new(params[:client])
-    puts @survey.name
-
+    @survey = Survey.create(survey_params)
+    puts '>>> new Survey Created with Name = ' + @survey.name
   end
+
+  private
+    def survey_params
+      params.require(:survey).permit(:name)
+    end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,2 @@
 class WelcomeController < ApplicationController
-  def create
-    @survey = Survey.new()
-  end
 end
