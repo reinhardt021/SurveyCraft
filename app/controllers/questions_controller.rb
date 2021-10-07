29aa5c0d3170a16fec29305f9b8faa62e283440e
Reinhardt <reinhardt.cgr@gmail.com>
2021-10-07 23:36:22 +1300

    removed logging for debugging purposes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,22 +7,12 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    puts '>>> TEST in QuestionsController@create'
-    # TODO: add logic here to store the question in the Survey and reload the new page
     @question = Question.new(question_params)
     @survey.questions << @question
-    puts '>>> SURVEY = ' + @survey.name
-    puts '>>> new QUESTION = ' + @question.question
-    puts '>>> new QUESTION survey = ' + @question.survey_id.to_s
     if @question.save
-      puts '>>> QUESTION saved'
       @question = @survey.questions.build
       redirect_to action: "new"
     else 
-      puts '>>> QUESTION ERROR'
-      @question.errors.full_messages.each do |message|
-        puts message
-      end
       redirect_to action: "new"
       flash[:error] = "Question could not save"
     end
