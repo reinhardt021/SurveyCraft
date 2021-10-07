766be583a2b807edc195b8b91bc8d232f2c61ae5
Reinhardt <reinhardt.cgr@gmail.com>
2021-10-07 23:27:05 +1300

    finally have the question saving to the survey now

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,11 +7,33 @@ class QuestionsController < ApplicationController
   end
 
   def create
+    puts '>>> TEST in QuestionsController@create'
     # TODO: add logic here to store the question in the Survey and reload the new page
+    @question = Question.new(question_params)
+    @survey.questions << @question
+    puts '>>> SURVEY = ' + @survey.name
+    puts '>>> new QUESTION = ' + @question.question
+    puts '>>> new QUESTION survey = ' + @question.survey_id.to_s
+    if @question.save
+      puts '>>> QUESTION saved'
+      @question = @survey.questions.build
+      redirect_to action: "new"
+    else 
+      puts '>>> QUESTION ERROR'
+      @question.errors.full_messages.each do |message|
+        puts message
+      end
+      redirect_to action: "new"
+      flash[:error] = "Question could not save"
+    end
   end
 
   private
     def get_survey
       @survey = Survey.find(params[:survey_id])
+    end
+    
+    def question_params
+      params.require(:question).permit(:question, :survey_id)
     end
 end
