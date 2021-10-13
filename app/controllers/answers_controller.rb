10236a85c957950d15716bba0b1778846646612b
Reinhardt <reinhardt.cgr@gmail.com>
2021-10-13 22:22:48 +1300

    WIP for the answers create route

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,28 @@
+require 'securerandom'
+
+class AnswersController < ApplicationController
+  def create
+    # // POST surveys/{survey_id}/answers
+    # {
+    #   76: {
+    #     question_id: 76,
+    #     answer: yes
+    #   },
+    #   77: {
+    #     question_id: 76,
+    #     answer: yes
+    #   },
+    # ]
+    @survey = Survey.new(params[:id])
+    # TODO: change name to session ID
+    #@answer.uuid = SecureRandom.uuid
+    @uuid = SecureRandom.uuid
+    puts @survey
+    puts @uuid
+    puts params
+    #if @survey.save
+      #redirect_to new_survey_question_path(@survey)
+    #end
+  end
+
+end
