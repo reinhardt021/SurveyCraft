549c5a2a563971be8a66e8dbcbfb103e946c1c6f
Reinhardt <reinhardt.cgr@gmail.com>
2021-10-20 23:28:15 +1300

    removed dead code

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,35 +2,16 @@ class AnswersController < ApplicationController
   def new 
     @survey = Survey.find(params[:survey_id])
     @question = Question.find(params[:question_id])
-    # start a new answer for the question
-    #check for session_id in query params
-    #puts params[:session_id]
+    # TODO: check for session_id in query params
     @answer = @question.answers.build(session_id: params[:session_id])
-    puts '>> the session id'
-    puts params[:session_id]
-    #@answer = Answer.new(session_id: params[:session_id])
   end
 
   def create
-    # // POST surveys/{survey_id}/questions/{question}/answers
-    #   {
-    #     question_id: 76,
-    #     answer: yes
-    #   }
-    #@survey = Survey.find(params[:survey_id])
+    # TODO: error out if there aren't any session_id
     @question = Question.find(params[:question_id])
-    #@answer = @question.answers.build(session_id: params[:session_id])
-    puts '>> answers_controller@create session id'
-    puts params[:session_id]
     @answer = @question.answers.build(answer_params)
-    #@answer = Answer.new(question_id: params[:question_id], session_id: params[:session_id], answer: params[:answer])
-    # TODO: change name to session ID
-    #@answer.uuid = SecureRandom.uuid
-    puts @survey
-    puts @uuid
-    puts params
     if @answer.save
-      puts '>>>> CREATED ANSWER'
+      # TODO: redirect to the next question
       #redirect_to new_survey_question_path(@survey)
     end
   end
