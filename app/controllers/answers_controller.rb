32d6119a69cc36b9b41d2233d8fb97ff4df67ee3
Reinhardt <reinhardt.cgr@gmail.com>
2021-10-27 22:56:02 +1300

    commented out the unnecessary code now that we use submissions controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,22 +1,22 @@
 class AnswersController < ApplicationController
-  def new 
-    @survey = Survey.find(params[:survey_id])
-    @question = Question.find(params[:question_id])
+  #def new 
+    #@survey = Survey.find(params[:survey_id])
+    #@question = Question.find(params[:question_id])
     # TODO: check for session_id in query params
-    @answer = @question.answers.build(session_id: params[:session_id])
-  end
+    #@answer = @question.answers.build(session_id: params[:session_id])
+  #end
 
-  def create
+  #def create
     # TODO: error out if there aren't any session_id
-    @question = Question.find(params[:question_id])
-    @answer = @question.answers.build(answer_params)
-    if @answer.save
+    #@question = Question.find(params[:question_id])
+    #@answer = @question.answers.build(answer_params)
+    #if @answer.save
       # TODO: redirect to the next question
       #redirect_to new_survey_question_path(@survey)
-    end
-  end
+    #end
+  #end
 
-  def answer_params
-    params.require(:answer).permit(:question_id, :session_id, :answer)
-  end
+  #def answer_params
+    #params.require(:answer).permit(:question_id, :session_id, :answer)
+  #end
 end
