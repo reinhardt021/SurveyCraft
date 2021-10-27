b36c5a425930c45e7128b518255eb7a722387578
Reinhardt <reinhardt.cgr@gmail.com>
2021-10-27 23:14:01 +1300

    WIP closer but have the nested answers attributes kind of messed. will need more tinkering

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -11,11 +11,18 @@ class SubmissionsController < ApplicationController
   def create
     puts '>>> SubmissionsController'
     puts params
-    @survey = Survey.find(params[:survey_id])
-    @answer = @question.answers.build(answer_params)
+    #@survey = Survey.find(params[:survey_id])
+    #@answer = @question.answers.build(answer_params)
+    @submission = Submission.new(submission_params)
+    puts '>>> Submission'
+    puts @submission
     #if @answer.save
       # TODO: redirect to the next question
       #redirect_to new_survey_question_path(@survey)
     #end
   end
+  private
+    def submission_params
+      params.require(:submission).permit(:survey_id, answer: [:question_id, :answer])
+    end
 end
