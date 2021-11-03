b1964c38c804aa400b6611e9ac9995f77ad0b206
Reinhardt <reinhardt.cgr@gmail.com>
2021-11-03 21:41:19 +1300

    finally have the survey submission working as wanted now

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -9,20 +9,26 @@ class SubmissionsController < ApplicationController
   # TODO: Test Driven Design  = WRITE TESTS
 
   def create
-    puts '>>> SubmissionsController'
-    puts params
-    #@survey = Survey.find(params[:survey_id])
-    #@answer = @question.answers.build(answer_params)
-    @submission = Submission.new(submission_params)
-    puts '>>> Submission'
-    puts @submission
-    #if @answer.save
-      # TODO: redirect to the next question
-      #redirect_to new_survey_question_path(@survey)
-    #end
+    @survey = Survey.find(params[:survey_id])
+    @submission = @survey.submissions.build(submission_params)
+    puts ">>> Submission@survey_id=#{@submission.survey_id}"
+    @submission.answers.each do |answer|
+      puts ">>> answer.question_id=#{answer.question_id}"
+      puts ">>> answer.question.question=#{answer.question.question}"
+      puts ">>> answer.answer=#{answer.answer}"
+    end
+    if @submission.save
+      puts '>>> Submission saved'
+      puts '>>> check if answers saved'
+      # TODO: redirect to suvery show page
+    else 
+      puts '>>> submission did not save'
+      #why is this just pushing me to http://localhost:3000/surveys/11/submissions
+      render :new
+    end
   end
   private
     def submission_params
-      params.require(:submission).permit(:survey_id, answer: [:question_id, :answer])
+      params.require(:submission).permit(:survey_id, answers_attributes: [:question_id, :answer])
     end
 end
