ecbd2f0d6a1daa3d96c62047d7a7669344c4c2cf
Reinhardt <reinhardt.cgr@gmail.com>
2021-11-11 13:08:58 +1300

    created model method to easily check in views if survey has questions

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -4,4 +4,8 @@ class Survey < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :answers, through: :questions
   accepts_nested_attributes_for :questions
+
+  def has_questions?
+    questions.count > 0
+  end
 end
