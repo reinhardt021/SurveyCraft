cac8fa2d05a0874424ccba8f2f63f7c966be45ca
Reinhardt <reinhardt.cgr@gmail.com>
2021-11-11 13:32:18 +1300

    created helper method to simplify getting survey

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,29 +1,31 @@
 class SubmissionsController < ApplicationController
   def new 
-    @survey = Survey.find(params[:survey_id])
-    @submission = @survey.submissions.build
-    @survey.questions.each do |question|
+    @submission = survey.submissions.build
+    survey.questions.each do |question|
       @submission.answers.build(question_id: question.id)
     end
   end
   # TODO: Test Driven Design  = WRITE TESTS
 
   def create
-    @survey = Survey.find(params[:survey_id])
-    @submission = @survey.submissions.build(submission_params)
+    @submission = survey.submissions.build(submission_params)
     if @submission.save
-      redirect_to survey_submission_path(@survey, @submission)
+      redirect_to survey_submission_path(survey, @submission)
     else 
       render :new
     end
   end
 
   def show
-    @survey = Survey.find(params[:survey_id])
+    survey
     @submission = Submission.find(params[:id])
   end
 
   private
+    def survey 
+      @survey ||= Survey.find(params[:survey_id])
+    end
+
     def submission_params
       params.require(:submission).permit(:survey_id, answers_attributes: [:question_id, :answer])
     end
